6dbf673b5db0384723f67bba0480b209104d7949
Flavian Alexander <alexandruflavian23@gmail.com>
2014-01-07 23:14:41 +0000

    Added missing browsers.

---
+++ config/int.json.js
@@ -0,0 +1,263 @@
+var json = {
+    "Firefox4.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "4",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox5.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "5",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox6.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "6",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox7.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "7",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox8.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "8",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox9.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "9",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox10.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "10",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox11.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "11",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox12.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "12",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox13.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "13",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox14.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "14",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox15.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "15",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox16.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "16",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox17.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "17",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox18.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "18",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox19.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "19",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox20.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "20",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox21.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "21",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox22.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "22",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox23.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "23",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox24.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "24",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Firefox25.0 OSX Snow Leopard": {
+      "browserFamily": "Firefox",
+        "browserMajor": "25",
+        "browserMinor": "0",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    // Google Chrome Mac OS X Snow Leopard
+    "Chrome14 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "14",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome15 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "15",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome16 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "16",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome17 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "17",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome18 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "18",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome19 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "19",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome20 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "20",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome21 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "21",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome22 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "22",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome23 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "23",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome24 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "24",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome25 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "25",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome26 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "26",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome27 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "27",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome28 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "28",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome29 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "29",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome30 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "30",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Chrome31 OSX Snow Leopard": {
+      "browserFamily": "Chrome",
+        "browserMajor": "31",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+
+    // Opera Mac OS X Snow Leopard
+    "Opera11.6 OSX Snow Leopard": {
+      "browserFamily": "Opera",
+        "browserMajor": "11",
+        "browserMinor": "6"
+    },
+    "Opera12.10 OSX Snow Leopard": {
+      "browserFamily": "Opera",
+        "browserMajor": "12",
+        "browserMinor": "10",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Opera12.14 OSX Snow Leopard": {
+      "browserFamily": "Opera",
+        "browserMajor": "12",
+        "browserMinor": "14",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Opera12.15 OSX Snow Leopard": {
+      "browserFamily": "Opera",
+        "browserMajor": "12",
+        "browserMinor": "15",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    "Opera12.16 OSX Snow Leopard": {
+      "browserFamily": "Opera",
+        "browserMajor": "12",
+        "browserMinor": "16",
+        "osFamily": "Mac OS X Snow Leopard"
+    },
+    // Safari Mac OS X Snow Leopard
+    "Safari 5.1 OSX Snow Leopard": {
+      "browserFamily": "Safari",
+        "browserMajor": "5",
+        "browserMinor": "1",
+        "osFamily": "Mac OS X Snow Leopard"
+    }
+}
